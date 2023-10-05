80349307efde9f69918e3f0334b9c0749f66b76d
Quasark <q@quasark.xyz>
2023-10-04 17:06:14 -0700

    mostly working

--- vendor/vendor.js
+++ vendor/vendor.js
@@ -1,5 +1,4 @@
 'use strict';
-
 require('dotenv').config();
 const PORT = process.env.PORT || 3002;
 const SERVER_URL = process.env.SERVER_URL + ':' + PORT + '/caps' || 'http://localhost:3002/caps';
@@ -7,32 +6,25 @@ const SERVER_URL = process.env.SERVER_URL + ':' + PORT + '/caps' || 'http://loca
 const io = require('socket.io-client');
 const socket = io.connect(SERVER_URL);
 
-function placeOrder(orderDetails) {
-  // console.log('about to connect to server', orderDetails)
+function placeOrderForStore(storeName, orderDetails) {
+  orderDetails.storeName = storeName;
   socket.emit('join', orderDetails);
   console.log('Vendor has joined the room: ', orderDetails.storeName);
   socket.emit('pickup', orderDetails);
+  socket.emit('getAll', { clientId: orderDetails.storeName, eventName: 'delivered' });
 }
 
 socket.on('delivered', payload => {
   console.log(`VENDOR: Thank you for delivering ${payload.orderId}`);
-  process.exit(0); 
+  socket.emit('received', payload);
 });
 
+socket.on('messages', payload => {
+  console.log(`VENDOR: Received message ${payload.messageId}`);
+});
 
+socket.on('get-messages', payload => {
+  console.log(`VENDOR: Received messages ${payload}`);
+});
 
-
-
-///////////////////////////////////
-// const events = require('../eventPool');
-// 
-// function placeOrder(orderDetails) {
-//   events.emit('pickup', orderDetails);
-// }
-// 
-// events.on('delivered', payload => {
-//   console.log(`VENDOR: Thank you for delivering ${payload.orderId}`);
-// });
-///////////////////////////////////
-
-module.exports = { placeOrder };
+module.exports = { placeOrderForStore };
